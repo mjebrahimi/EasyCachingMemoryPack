58bd1b59dbf93c7eb076b0ef5b0c70ba1fad7834
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-07-06 18:53:07 +0330

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using EasyCaching.Core.Serialization;
+using EasyCaching.Serialization.MemoryPack;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services.AddEasyCaching(options => options.WithMemoryPack());
+
+var provider = services.BuildServiceProvider();
+
+var serializer = provider.GetRequiredService<IEasyCachingSerializer>();
+//Throws => System.MissingMethodException: Method not found: 'Void MemoryPack.MemoryPackSerializerOptions.set_StringEncoding(MemoryPack.StringEncoding)'.
+
+Console.WriteLine("Finished Successfully");
